9afabc33bf3c1775d917d65739dd3bb9dbc7476c
agungsapp <agung.dni19@gmail.com>
2023-12-16 13:16:57 +0700

    finish front end

--- backend/index.js
+++ backend/index.js
@@ -38,6 +38,6 @@ app.use((err, req, res, next) => {
 });
 
 // Menjalankan server
-app.listen(3000, () => {
-  console.log('Server is running on port 3000!');
+app.listen(5000, () => {
+  console.log('Server is running on port 5000!');
 });
